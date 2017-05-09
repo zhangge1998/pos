8da918c6b7ebf580b9cccc2ff07f00231f21f923
zhangge1998 <1263061442@qq.com>
2017-05-09 20:13:40 +0800

    code by tasking

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,41 +1,85 @@
 'use strict';
+
 function printReceipt(inputs) {
-  var sum=0,a_sum;
-  var a_free=0;
-  var s='***<没钱赚商店>收据***';
-  var All_Item=loadAllItems();
-  var c=loadPromotions();
-  for(var i=0;i<All_Item.length;i++)
-  {
-    var temp=All_Item[i];
-    temp.count=0;
-    for(var j=0;j<inputs.length;j++)
-    {
-      if(temp.barcode==inputs[j].substring(0,10))
-      {
-        if(inputs[j].length==10)
-        temp.count+=1;
-        else
-          temp.count+=parseFloat(inputs[j].substring(11));
+  var allItems = loadAllItems();
+  var cartItemCounts = buildCounts(allItems, inputs);
+  var cartItemStatus = buildItemStatus(cartItemCounts);
+  var totals = buildTotalAndDiscount(cartItemStatus);
+  var string = printReceiptText(totals);
+  console.log(string);
+}
+
+function buildCounts(allItems, inputs) {
+  var cartItemCounts = [];
+  for (var i = 0; i < allItems.length; i++) {
+    var count = isExit(allItems[i], inputs);
+    cartItemCounts.push({item: allItems[i], count: count});
+  }
+  return cartItemCounts;
+}
+
+function isExit(allItems, inputs) {
+  var count = 0;
+  for (var i = 0; i < inputs.length; i++) {
+    if (allItems.barcode === inputs[i].substring(0, 10))
+      count += elementCount(inputs[i]);
+  }
+  return count;
+}
+
+function elementCount(item) {
+  if (item.length === 10)
+    return 1;
+  else {
+    var cCount = item.split("-");
+    return parseFloat(cCount[1]);
+  }
+}
+
+function buildItemStatus(cartItemCounts) {
+  var cartItemStatus = [];
+  var promotions = loadPromotions();
+  for (var i = 0; i < cartItemCounts.length; i++) {
+    var temp = cartItemCounts[i];
+    if (cartItemCounts[i].count > 0) {
+      if (isPromote(cartItemCounts[i], promotions)) {
+        var subtotal = temp.item.price * (temp.count - 1);
+        cartItemStatus.push({cartItem: cartItemCounts[i], subtotal: subtotal, itemDiscount: temp.item.price});
       }
-    }
-    if(temp.count!=0)
-    {
-      for(var k=0;k<c[0].barcodes.length;k++)
-      {
-        if (temp.barcode == c[0].barcodes[k]) {
-          a_sum=(temp.count-parseInt(temp.count/3))*temp.price;
-          a_free=a_free+(temp.count/3)*temp.price;
-        }
-        else
-          a_sum = temp.count* temp.price;
+      else {
+        var subtotal1 = temp.item.price * temp.count;
+        cartItemStatus.push({cartItem: cartItemCounts[i], subtotal: subtotal1, itemDiscount: 0});
       }
-      //a_sum = temp.count* temp.price;
-      sum += a_sum;
-      s += '\n' + '名称：' + temp.name + '，' + '数量：' + temp.count + temp.unit + '，' + '单价：' +
-        temp.price + '(元)' + '，' + '小计：' + a_sum + '(元)';
     }
   }
-  s+='\n'+'----------------------'+'\n'+'总计：'+sum+'(元)'+'\n'+'节省：'+a_free+'（元）'+'\n'+'**********************';
-  console.log(s);
+  return cartItemStatus;
 }
+
+function isPromote(cartItemCounts, promotions) {
+  for (var i = 0; i < promotions[0].barcodes.length; i++) {
+    if (cartItemCounts.item.barcode === promotions[0].barcodes[i] && cartItemCounts.count % 2 != 0) {
+      return 1;
+    }
+  }
+}
+
+function buildTotalAndDiscount(cartItemStatus) {
+  var total = 0;
+  var discount = 0;
+  for (var i = 0; i < cartItemStatus.length; i++) {
+    total += cartItemStatus[i].subtotal;
+    discount += cartItemStatus[i].itemDiscount;
+  }
+  return {receiptItem: cartItemStatus, total: total, discount: discount};
+}
+
+function printReceiptText(totals) {
+  var string = '***<没钱赚商店>收据***';
+  for (var k = 0; k < totals.receiptItem.length; k++) {
+    var object = totals.receiptItem[k];
+    string += '\n' + '名称：' + object.cartItem.item.name + '，' + '数量：' + object.cartItem.count + object.cartItem.item.unit + '，' + '单价：' + object.cartItem.item.price.toFixed(2) + '(元)' + '，' + '小计：' + object.subtotal.toFixed(2) + '(元)';
+  }
+  string += '\n' + '----------------------' + '\n' + '总计：' + totals.total.toFixed(2) + '(元)' + '\n' + '节省：' + totals.discount.toFixed(2) + '(元)' + '\n' + '**********************';
+  return string;
+}
+
