f7ef02849a3e74a9f05d3f3fd741f4d760f748b3
zhangge1998 <1263061442@qq.com>
2018-03-14 21:41:22 +0800

    use ES6

--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -1,34 +1,34 @@
 'use strict';
-function printReceipt(inputs) {
-  var itemTotals = buildSubtotal(inputs);
-  var totals = buildTotal(itemTotals);
-  var string = printReceiptText(totals);
+let printReceipt=(inputs)=>{
+  let itemTotals = buildSubtotal(inputs);
+  let totals = buildTotal(itemTotals);
+  let string = printReceiptText(totals);
   console.log(string);
-}
+};
 
-function buildSubtotal(inputs) {
-  var itemTotals = [];
-  for (var i = 0; i < inputs.length; i++) {
-    var subtotal = inputs[i].price * inputs[i].count;
-    itemTotals.push({item: inputs[i], subtotal: subtotal});
+let buildSubtotal=(inputs)=> {
+  let itemTotals = [];
+  for (let i of inputs) {
+    let subtotal = i.price * i.count;
+    itemTotals.push({item: i, subtotal: subtotal});
   }
   return itemTotals;
-}
+};
 
-function buildTotal(itemTotals) {
-  var total = 0;
-  for (var j = 0; j < itemTotals.length; j++) {
-    total += itemTotals[j].subtotal;
+let buildTotal=(itemTotals)=> {
+  let total = 0;
+  for (let j of itemTotals) {
+    total += j.subtotal;
   }
   return {receiptItem: itemTotals, total: total};
-}
+};
 
-function printReceiptText(totals) {
-  var  string= '***<没钱赚商店>收据***';
-  for (var k = 0; k < totals.receiptItem.length; k++) {
-    var object = totals.receiptItem[k];
+let printReceiptText=(totals)=> {
+  let  string= '***<没钱赚商店>收据***';
+  for (let k of totals.receiptItem) {
+    let object = k;
     string += '\n' + '名称：' + object.item.name + '，' + '数量：' + object.item.count + object.item.unit + '，' + '单价：' + object.item.price.toFixed(2) + '(元)' + '，' + '小计：' + object.subtotal.toFixed(2) + '(元)';
   }
   string += '\n' + '----------------------' + '\n' + '总计：' + totals.total.toFixed(2) + '(元)' + '\n' + '**********************';
   return string;
-}
+};
